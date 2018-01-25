0958f9f618dc0de6da54c9b41d9f6635d95bc19d
ming19871211 <ming19871211@163.com>
2018-01-25 17:22:19 +0800

    增加测试模块

---
+++ src/test/java/com/mqm/boot/biz/controller/SampleControllerTest.java
@@ -0,0 +1,20 @@
+package com.mqm.boot.biz.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleControllerTest {
+
+	@Test
+	public void testHome() {
+		Assert.assertEquals(5, 5);
+	}
+
+}
